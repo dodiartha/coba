316e168041541b78da7543c9e8b3ebaeba113df5
dodiartha <dodiartha46@gmail.com>
2018-03-20 08:32:41 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View
+} from 'react-native';
+// import HitungVolumeBalok from './src/HitungVolumeBalok';
+import Sales from './src/Sales';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.containerMain}>
+        <Sales />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerMain: {
+    paddingTop: 20,
+    flex: 1,
+    backgroundColor: '#2196F3'
+  }
+})
+
